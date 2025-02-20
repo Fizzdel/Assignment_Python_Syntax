26b6d703a636e8156899e7b9a3b7d5b25c30b01b
Fizzdel <fid.gen.fig@gmail.com>
2025-02-20 13:48:03 -0600

    assignment 1 done

---
+++ assignment1.py
@@ -0,0 +1,13 @@
+#Task 1
+weather = "sunny"
+if weather == "sunny":
+    print('wear sunglasses')
+else:
+    print('take umbrella!')
+
+#Task 2
+Mood=input("how are you feeling today?")
+if Mood =="happy":
+    print("thats great to hear")
+elif Mood=="sad":
+    print("I hope your day gets better!")
---
+++ assignment3.py
@@ -0,0 +1,9 @@
+#Task1
+bread,cheese,ham=(10,2.5,3.5)
+print(bread+cheese+ham)
+print(type(bread+cheese+ham))
+
+#Task2
+initial_amount=10000
+INTREST_RATE=4.5
+print(initial_amount+(initial_amount*(INTREST_RATE*.01)))
